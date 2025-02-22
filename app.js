16cb3cb5c846115f20ad43e68847874638511011
Mayran Adriel <mayranalbuquerque@gmail.com>
2025-02-22 19:08:39 -0300

    ex finalizado

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const listaDeNomes = document.querySelector(".name-list");
+const resultado = document.querySelector(".result-list");
+const botaoAdicionar = document.querySelector(".button-add");
+const inputNome = document.querySelector(".input-name");
+const botaoSortear = document.querySelector(".button-draw");
+
+let listaDeAmigos = [];
+
+botaoAdicionar.addEventListener("click", () => {
+    const valorInput = inputNome.value;
+    if (valorInput) {
+        listaDeNomes.innerHTML = "";
+        listaDeAmigos = adicionarNaListaDeAmigos(valorInput, listaDeAmigos);
+        listaDeAmigos.forEach(amigoAtual => {
+            const li = document.createElement("li");
+            li.textContent = amigoAtual;
+            listaDeNomes.appendChild(li);
+        })
+    }
+    else {
+        avisoInputInvalido();
+    }
+});
+
+botaoSortear.addEventListener("click", () => {
+    const amigoSorteado = sorteiaAmigo(listaDeAmigos);
+    const mensagem = `O amigo secreto sorteado é: ${amigoSorteado}`
+    resultado.textContent = mensagem;
+})
+
+const avisoInputInvalido = () => {
+    const mensagem = "Por favor insira um nome válido!"
+    alert(mensagem)
+}
+
+const adicionarNaListaDeAmigos = (amigo, listaAmigos = []) => {
+
+    return [...listaAmigos, amigo]
+}
+
+const sorteiaAmigo = (listaAmigos = []) => {
+
+    const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+
+    return listaAmigos[indiceAleatorio];
+}
+
+
